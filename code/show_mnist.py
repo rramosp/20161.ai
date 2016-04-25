8d40fda7c2cea813602bdae7dbc0bc6694728e4b
Raul Ramos Pollan <rlx@rlximac.local>
2016-04-25 14:14:58 -0500

    first commit

---
+++ code/show_mnist.py
@@ -0,0 +1,12 @@
+def show_sample_mnist(d,c):
+    perm = np.random.permutation(range(d.shape[0]))[0:50]
+    random_imgs   = d[perm]
+    random_labels = c[perm] 
+    fig = plt.figure(figsize=(10,6))
+    img_size = int(np.sqrt(d.shape[1]))
+    for i in range(random_imgs.shape[0]):
+        ax=fig.add_subplot(5,10,i+1)
+        plt.imshow(random_imgs[i].reshape(img_size, img_size), interpolation="nearest", cmap = plt.cm.Greys_r)
+        ax.set_title(int(random_labels[i]))
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
